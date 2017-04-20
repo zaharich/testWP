78b0634a1757323b4f9ded4e1c62a3aeba32d1d5
zaharichh <zaharichh@gmail.com>
2017-04-20 17:03:53 +0300

    20.04.17

--- index.php
+++ index.php
@@ -4,20 +4,15 @@
     <main class="row col-md-9">
         <!-- first loop-->
         <?php while ( have_posts() ) : the_post(); ?>
-            <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                <header>
-                    <h2>
-                        <a href="<?php the_permalink(); ?>" rel="bookmark"> <?php the_title(); ?> <?php the_post_thumbnail(); ?> </a>
-                    </h2>
-                    <?php the_content(); ?>
-                    <span> Posted on <?php the_time('j F Y'); ?> by <?php the_author_posts_link(); ?> </span>
-                </header>
-            </article>
+
+            <?php if( in_category('winter') || in_category('spring') ){ ?>
+                <?php get_template_part( 'template-parts/post/content', get_post_format() ); ?>
+            <?php } ?>
+
         <?php endwhile;
         wp_reset_postdata(); ?>
 
         <!--second loop-->
-        <hr>
         <?php
         global $post;
         $args = array('category' => get_cat_ID('summer') . "," . get_cat_ID('autumn'));
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -6,60 +6,17 @@
  * Time: 10:51
  */ ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php
-if ( is_sticky() && is_home() ) :
-    echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
-endif;
-?>
-<header class="entry-header">
-    <?php
-    if ( 'post' === get_post_type() ) :
-        echo '<div class="entry-meta">';
-        if ( is_single() ) :
-            twentyseventeen_posted_on();
-        else :
-            echo twentyseventeen_time_link();
-            twentyseventeen_edit_link();
-        endif;
-        echo '</div><!-- .entry-meta -->';
-    endif;
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+    
+    <header>
+        <h2>
+            <a href="<?php the_permalink(); ?>" rel="bookmark"> <?php the_title(); ?> <?php the_post_thumbnail(); ?> </a>
+        </h2>
+        <?php the_content(); ?>
 
-    if ( is_single() ) {
-        the_title( '<h1 class="entry-title">', '</h1>' );
-    } else {
-        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-    }
-    ?>
-</header><!-- .entry-header -->
-
-<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
-    <div class="post-thumbnail">
-        <a href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
-        </a>
-    </div><!-- .post-thumbnail -->
-<?php endif; ?>
-
-<div class="entry-content">
-    <?php
-    /* translators: %s: Name of current post */
-    the_content( sprintf(
-        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-        get_the_title()
-    ) );
-
-    wp_link_pages( array(
-        'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-        'after'       => '</div>',
-        'link_before' => '<span class="page-number">',
-        'link_after'  => '</span>',
-    ) );
-    ?>
-</div><!-- .entry-content -->
-
-<?php if ( is_single() ) : ?>
-    <?php twentyseventeen_entry_footer(); ?>
-<?php endif; ?>
+        <i><span class="post-span"> Posted on <?php the_time('j F Y'); ?> by <?php the_author_posts_link(); ?> </span></i>
+        <br>
+    </header>
 
 </article><!-- #post-## -->
+<br>
