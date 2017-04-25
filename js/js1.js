ad0f384d5de3d61821e063a0ab48316f7bf8a12d
zaharichh <zaharichh@gmail.com>
2017-04-25 17:10:23 +0300

    25.04.17

--- js/js1.js
+++ js/js1.js
@@ -1,10 +1,16 @@
 /**
  * Created by TUPOLEV on 07.04.2017.
  */
-//$('#calendar').datepicker();
-var heightHead = 440;
-var menuElem = $('#top-menu');
+jQuery(function($) {
+    //$('#calendar').datepicker();
+    var heightHead = 440;
+    var menuElem = $('#top-menu');
 
-$(window).scroll(function () {
-    menuElem.css('background-color', red);
+    $(window).scroll(function () {
+        var windowTop = $(this).scrollTop();
+        if(windowTop > heightHead)
+            menuElem.css({'position': 'fixed', 'top': '0', 'background-color':'rgb(0, 191, 255)'});
+        else
+            menuElem.css({'position':'relative', 'background-color':'rgba(0, 191, 255, 0.1'});
+    });
 });
